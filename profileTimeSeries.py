c94499ec4cb3662c32f4ac535334bdee53af3785
Arnoldas Kurbanovas <akurbanovas@albany.edu>
2017-01-27 16:55:44 -0500

    adding the profile Time Series file

---
+++ profileTimeSeries.py
@@ -0,0 +1,8 @@
+'''
+This file uses the datafram with time index
+Main use is for plotting heatmaps using matplotlib
+Includes the core functions handling the data
+for plotting and agrigating by time
+'''
+
+
