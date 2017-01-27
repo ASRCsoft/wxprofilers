92d94853fd01dfb5b345b45f5c49cbb11079a236
Will May <williamcmay@live.com>
2017-01-27 17:12:14 -0500

    Added profileInstrument.py, fixed typo in profileTimeSeries.py

--- profileInstrument.py
+++ profileInstrument.py
@@ -1,1 +1,8 @@
 class ProfileInstrument:
+
+    def __init__(self, file = None):
+        # takes a netCDF file, organizes data
+        self.data = {}
+
+    def export(self):
+        # writes a CF-compliant netCDF file containing the profileInstrument data
--- profileTimeSeries.py
+++ profileTimeSeries.py
@@ -4,5 +4,6 @@ Main use is for plotting heatmaps using matplotlib
 Includes the core functions handling the data
 for plotting and agrigating by time
 '''
+import pandas as pd
 
-
+class ProfileTimeSeries(pd.DataFrame):
