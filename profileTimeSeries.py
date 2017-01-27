664e2e6c13cdd87f1702f0bf0a9f68f65e409303
Will May <williamcmay@live.com>
2017-01-27 16:58:11 -0500

    Added profileInstrument.py, fixed typo in profileTimeSeries.py

--- profileTimeSeries.py
+++ profileTimeSeries.py
@@ -1,5 +1,5 @@
 '''
-This file uses the datafram with time index
+This file uses the dataframe with time index
 Main use is for plotting heatmaps using matplotlib
 Includes the core functions handling the data
 for plotting and agrigating by time
