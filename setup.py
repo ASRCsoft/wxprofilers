93db5d8f83cc59321a7219ab1b8f855bfa056d09
William May <williamcmay@live.com>
2019-04-17 21:55:33 -0400

    Fix extremely confused setup in setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,5 @@
-#!/usr/bin/env python
-
 from __future__ import absolute_import
-from distutils.core import setup
 from Cython.Build import cythonize
-
-
-setup(name='raspPy',
-      version='0.1dev',
-      description='Utilities for working with weather profile data with xarray',
-      url='https://github.com/ASRCsoft/ipas_python',
-      author='William May',
-      author_email='williamcmay@live.com',
-      packages=['rasppy'],
-      test_suite='nose.collector',
-      tests_require=['nose'],
-      install_requires=[
-          'cython',
-          'matplotlib',
-          'xarray',
-          'metpy',
-          'statsmodels',
-          'nipy'
-      ]
-)
 
 
 def configuration(parent_package='', top_path=None):
@@ -39,4 +16,20 @@ def configuration(parent_package='', top_path=None):
 
 if __name__ == '__main__':
     from numpy.distutils.core import setup
-    setup(**configuration(top_path='').todict())
+    setup(version='0.1dev',
+          description='Utilities for working with weather profile data with xarray',
+          url='https://github.com/ASRCsoft/ipas_python',
+          author='William May',
+          author_email='williamcmay@live.com',
+          test_suite='nose.collector',
+          tests_require=['nose'],
+          install_requires=[
+              'cython',
+              'matplotlib',
+              'xarray',
+              'metpy',
+              'statsmodels',
+              'nipy'
+          ],
+          **configuration(top_path='').todict()
+    )
