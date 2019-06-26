b19a0cf04f9f99ae60def44136b2090462006e1c
William May <williamcmay@live.com>
2019-06-26 01:12:43 -0400

    Mock wxprofilers for autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -287,4 +287,4 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
 
-autodoc_mock_imports = ['numpy', 'pandas']
+autodoc_mock_imports = ['numpy', 'pandas', 'wxprofilers']
