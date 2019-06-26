03495861b34d598a04872cd367edf4961b566495
wmay <williamcmay@live.com>
2019-06-26 14:17:58 -0400

    Remove autodoc package mocking

--- docs/conf.py
+++ docs/conf.py
@@ -286,5 +286,3 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
-
-autodoc_mock_imports = ['numpy', 'pandas', 'wxprofilers']
