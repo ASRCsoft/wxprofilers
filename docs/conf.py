cb0ab8b95121b02bdbdecc2575e5af757a256ee9
wmay <williamcmay@live.com>
2019-06-26 19:31:36 -0400

    Fix compiled build path for autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ import os
 sys.path.insert(0, os.path.abspath('..'))
 # also add path for compiled modules
 compiled_module_folder = [ f for f in os.listdir('../build') if f[:3] == 'lib' ][0]
-compiled_module_path = '../build/' + compiled_module_folder
+compiled_module_path = '../build/' + compiled_module_folder + '/wxprofilers'
 sys.path.insert(1, os.path.abspath(compiled_module_path))
 
 # -- General configuration ------------------------------------------------
