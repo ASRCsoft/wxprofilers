10aa94aca47997492ac23765c06919060a189262
wmay <williamcmay@live.com>
2019-06-26 20:38:12 -0400

    Try using installed package for autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -18,11 +18,11 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('..'))
+# sys.path.insert(0, os.path.abspath('..'))
 # also add path for compiled modules
-compiled_module_folder = [ f for f in os.listdir('../build') if f[:3] == 'lib' ][0]
-compiled_module_path = '../build/' + compiled_module_folder + '/wxprofilers'
-sys.path.insert(1, os.path.abspath(compiled_module_path))
+# compiled_module_folder = [ f for f in os.listdir('../build') if f[:3] == 'lib' ][0]
+# compiled_module_path = '../build/' + compiled_module_folder + '/wxprofilers'
+# sys.path.insert(1, os.path.abspath(compiled_module_path))
 print(sys.path)
 
 # -- General configuration ------------------------------------------------
