95b00c0353bd805ea88962e235775dbea3b906b1
William May <williamcmay@live.com>
2019-06-27 21:41:38 -0400

    Format decode_rrs docstring

--- wxprofilers/sonde/_rrs.py
+++ wxprofilers/sonde/_rrs.py
@@ -6,9 +6,10 @@ from wxprofilers import _rrs_decoder
 def decode_rrs(bufrin, outputs, outdir=''):
     '''Run the RRS Decoder program provided by NOAA.
 
-    bufrin (str): path to the radiosonde bufr file.
-    outputs (str): desired output files (see RRS Decoder documentation).
-    outdir (str): output directory.
+    Args:
+        bufrin (str): path to the radiosonde bufr file.
+        outputs (str): desired output files (see RRS Decoder documentation).
+        outdir (str): output directory.
     '''
     # try to make sure the path is handled correctly
     out = str(Path(outdir)) + '/'
